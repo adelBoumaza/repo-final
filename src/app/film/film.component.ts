cee17263a00a2a815da9b04ebd8bbaec7c5a242a
Boumaza Adel <aboumaza@SQLI3075.SQLI>
2018-05-28 10:22:21 +0200

    new Commit

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -10,6 +10,7 @@ import { FilmService } from './film.service';
 export class FilmComponent implements OnInit {
     private film: Film;
     private films: any;
+    private btnUpdate: boolean;
     constructor(private filmService: FilmService) { }
 
     private categories = ['Film d\'action‎ ', 'Film d\'animation‎',
@@ -20,9 +21,15 @@ export class FilmComponent implements OnInit {
                           'Film historique‎', 'Film d\'horreur‎', 'Film policier‎', 'Film politique‎',
                           'Film de science-fiction‎', 'Film à sketches‎', 'Western‎'
                         ];
+    private durrees  =   [10, 15, 20 , 25, 30, 35, 40,
+                        45, 50, 55, 60, 65, 70, 75, 80, 85, 90,
+                        95, 100, 105, 110, 115, 120, 125, 130,
+                        135, 140, 145, 150, 155, 160, 165, 170
+                        ];
 
     reset() {
-        this.film = new Film( 0, 0, '', '', '', '');
+        this.film = new Film( 0, 0, '', '', '', '', 105);
+        this.btnUpdate = false;
     }
     ngOnInit(): void {
         this.getAll();
@@ -48,5 +55,13 @@ export class FilmComponent implements OnInit {
         }, error => {
             console.log(error);
         });
+    }
+    editer(item)
+    {
+
+        this.btnUpdate = true;
+        this.film = new Film(item.note, item.prix, item.nomFilm, item.categorie, item.date, item.lieu, item.durree);
+        this.film._id = item.id;
+        window.scrollTo(0, 0);
     }
 }
--- src/app/film/film.model.ts
+++ src/app/film/film.model.ts
@@ -1,11 +1,13 @@
 export class Film {
+        private id: string;
         private  note: number;
         private  prix: number;
         private  nomFilm: string;
         private  categorie: string;
         private  date: string;
         private  lieu: string;
-        constructor(note: number, prix: number, nomFilm: string, categorie: string, date: string, lieu: string)
+        private  durree: number;
+        constructor(note: number, prix: number, nomFilm: string, categorie: string, date: string, lieu: string, durree: number )
         {
             this.note = note;
             this.prix = prix;
@@ -13,5 +15,10 @@ export class Film {
             this.categorie = categorie;
             this.date = date;
             this.lieu = lieu;
+            this.durree = durree;
+        }
+
+        public set _id(_id: string){
+            this.id = _id;
         }
 }
